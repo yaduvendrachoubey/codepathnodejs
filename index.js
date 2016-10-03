3a7e6ee67b5ba31ba95e7495d4ebb856d71ecfd6
yaduvendrachoubey <yaduvendrachoubeys@gmail.com>
2016-10-02 20:27:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ let url = require('url')
 let argv = require('yargs')
     .default('host', '127.0.0.1:8000')
     .argv
-
 let port = argv.port || (argv.host === '127.0.0.1' ? 8000 : 80)
 let destinationUrl = argv.url || url.format({
    protocol: 'http',
-   host: argv.host,
-   port
+   host: argv.host + ':' + port
 })
 
 let path = require('path')
@@ -23,7 +21,7 @@ let logStream = logPath ? fs.createWriteStream(logPath) : process.stdout
 
 
 http.createServer((req, res) => {
-        console.log("Proxying request from $req.host")
+        logStream.write("\n Recieved request from the Proxy Server \n")
         for (let header in req.headers) {
               res.setHeader(header, req.headers[header])
         }
@@ -39,6 +37,11 @@ http.createServer((req, res) => {
         url: destinationUrl,
         method: req.method
     }
+    if (req.headers['x-destination-url']) {
+        console.log('Overriding the destination url')
+    	options['url'] = req.headers['x-destination-url']
+    }
+    logStream.write('\n  Request Sending to Proxy Server => Destination URL : ' + destinationUrl +  '\n')
     logStream.write('\n' + JSON.stringify(req.headers) + '\n')
     req.pipe(logStream, {end: false})
     //req.pipe(request(options)).pipe(res)
@@ -50,4 +53,3 @@ http.createServer((req, res) => {
     outboundResponse.pipe(res)
 
 }).listen(8001)
-
